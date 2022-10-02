a602eeedab75ecbf0386ebfbeae3871666516d15
tearsxo <108863534+tearsxo@users.noreply.github.com>
2022-10-02 19:31:16 -0400

    Delete assets directory

--- assets/js/slider.js
+++
@@ -1,9 +0,0 @@
-var video = document.getElementById('video');
-var volumeControl = document.getElementById('vol-control');
-
-var setVolume = function () {
-	video.volume = this.value / 100;
-};
-
-volumeControl.addEventListener('change', setVolume);
-volumeControl.addEventListener('input', setVolume);
